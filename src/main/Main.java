3ea9c85cd69f7a35d52fc0ee4e4e54cbe4a89136
Hallar Khan <163777073+HallarKhann@users.noreply.github.com>
2026-01-21 15:11:04 +0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("===== Guest House Management System =====\n");
+        System.out.println("==== Guest House Management System =====\n");
 
        
         GuestHouse guestHouse = new GuestHouse(
